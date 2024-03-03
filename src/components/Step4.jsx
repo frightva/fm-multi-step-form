ab00eafa6581e6832b356ea59ce8f4c132fba283
Ilija <slamkovilija4@gmail.com>
2024-03-03 12:56:06 +0100

    Step 4 css

--- src/components/Step4.jsx
+++ src/components/Step4.jsx
@@ -2,23 +2,45 @@ import React from 'react'
 
 function Step4() {
   return (
-    <div className='form'>
+    <div className='form spacer-bottom'>
       <h1 className='text-accent-700 fs-primary-heading fw-bold padding-inline-1'>Finishing up</h1>
       <p className='fs-body fw-medium text-neutral-200 padding-inline-1'>Double-check everythin looks OK before confirming</p>
 
-      <div className='finishing-up-container'>
-        <div>
+      <div className='finishing-up-container padding-inline-1 '>
+        <div className='border-bottom | flex-space-between '>
           <div>
-            <h2 className='fw-medium text-accent-700 fs-450'>Arcade (Monthly)</h2>
+            <h2 className='fw-medium text-accent-700 fs-450 '>Arcade (Monthly)</h2>
             <button className='change-btn'>Change</button>
           </div>
           <div>
-            
+            <span className='fw-medium text-accent-700 fs-450'>$9/mo</span>
           </div>
-
         </div>
-        <div></div>
-        <div></div>
+
+        <div className='margin-top-1-5'>
+          <div className='flex-space-between'>
+            <p className='p-info-text | text-accent-700'>
+              Online service
+            </p>
+            <p>
+              +$1/mo
+            </p>
+          </div>
+          <div className='flex-space-between'>
+            <p className='p-info-text | text-accent-700'>
+              Online service
+            </p>
+            <p>
+              +$1/mo
+            </p>
+          </div>
+        </div>
+
+        <div className='flex-space-between'>
+          <p className='p-info-text'>Total (per month)</p>
+          <p className='text-accent-500 fw-bold fs-500'>+$12/mo</p>
+        </div>
+
       </div>
         
     </div>
